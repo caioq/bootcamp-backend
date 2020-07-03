79bdd54cfbb37db3e95e1a7828a24f309a43dffe
Caio <caiogqueiroz@gmail.com>
2020-07-03 12:42:39 -0300

    Listagem de prestadores

---
+++ src/modules/users/dtos/IFindAllProvidersDTO.ts
@@ -0,0 +1,4 @@
+// quais os dados preciso para criar um appointment?
+export default interface IFindAllProvidersDTO {
+  except_user_id?: string;
+}
--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -2,6 +2,7 @@ import { uuid } from 'uuidv4';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -9,21 +10,33 @@ class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
-    const user = this.users.find(user => user.id === id);
+    const user = this.users.find(item => item.id === id);
 
     return user;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = this.users.find(user => user.email === email);
+    const user = this.users.find(item => item.email === email);
 
     return user;
+  }
+
+  public async findAllProviders({
+    except_user_id,
+  }: IFindAllProvidersDTO): Promise<User[]> {
+    let { users } = this;
+
+    if (except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
     const user = new User();
 
-    Object.assign(user, {id: uuid()}, userData);
+    Object.assign(user, { id: uuid() }, userData);
 
     this.users.push(user);
 
