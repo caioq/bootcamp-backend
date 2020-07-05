6943c71c1c7e28a0a2722696b15a18154cc082e7
Caio <caiogqueiroz@gmail.com>
2020-07-05 16:36:09 -0300

    Rotas e controllers de availability

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -19,7 +19,7 @@ type IResponse = Array<{
 @injectable()
 class ListProviderDayAvailabilityService {
   constructor(
-    @inject('AppointmentRepository')
+    @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
