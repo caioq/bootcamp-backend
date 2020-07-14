d2415899e55cd7aeba3a45167a09da5908a0516e
Caio <caiogqueiroz@gmail.com>
2020-07-14 10:55:23 -0300

    Não aplicando rate limiter nas rotas de leitura de imagem

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,10 +16,10 @@ import '@shared/container';
 
 const app = express();
 
-app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter); // apos a leitura de imagens, para nao se aplicar a elas
 app.use(routes);
 
 app.use(errors());
